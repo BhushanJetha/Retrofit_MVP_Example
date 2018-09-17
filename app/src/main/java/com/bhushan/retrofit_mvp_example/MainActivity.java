014b466879887a3c4e5f934a173027d14cd27ea8
Bhushan Jetha <bhushan.jetha@digitas.com>
2018-09-17 15:16:22 +0530

    Initial commit

---
+++ app/src/main/java/com/bhushan/retrofit_mvp_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bhushan.retrofit_mvp_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
